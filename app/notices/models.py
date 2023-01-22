d028aa01e85441ac7f86181d1ba8542fbca97374
swelanauguste <swelanauguste@gmail.com>
2023-01-22 11:47:42 -0400

    status

--- app/notices/models.py
+++ app/notices/models.py
@@ -90,7 +90,7 @@ class Notice(models.Model):
         return reverse("notice-detail", kwargs={"slug": self.slug})
 
     def __str__(self):
-        return f"{self.title.title()} - ({self.status.status.upper()})"
+        return f"{self.title.title()}"
 
 
 class NoticeFile(models.Model):
