d646959c5337be0adc6417e93a57d30f34c8121c
swelanauguste <swelanauguste@gmail.com>
2023-02-01 10:55:50 -0400

    collect-static

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -13,6 +13,6 @@ fi
 
 python manage.py migrate
 # python manage.py createsuperuser --username king --email no-reply@mail.com --no-input
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 exec "$@"
